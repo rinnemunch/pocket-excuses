aa9705ef4200b635132248caf4261bebafed1732
Shaun Fulton <fulton_shaun@yahoo.com>
2025-05-26 02:24:41 -0400

    Set up basic Main method with hardcoded excuse

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; 
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string template = "I couldn't finish my homework because my {0} exploded";
+
+        string wildcard = "microwave";
+
+        string excuse = string.Format(template, wildcard);
+
+
+        //Output 
+        Console.WriteLine("Generated Excuse:");
+        Console.WriteLine(excuse);
+    }
+}
