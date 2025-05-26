b85577c4b9b3fd6ebf743a057af2ca94c147b7e4
Shaun Fulton <fulton_shaun@yahoo.com>
2025-05-26 02:53:21 -0400

    Added categories with random excuse templates

--- Program.cs
+++ Program.cs
@@ -1,23 +1,49 @@
-﻿using System; 
-
+﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string template = "I couldn't finish my homework because my {0} exploded";
+        var excuseTemplates = new Dictionary<string, List<string>>
+        {
+            { "school", new List<string>
+                {
+                    "I couldn’t finish my homework because my {0} exploded.",
+                    "I got locked out after chasing my {0} down the street.",
+                    "My {0} caught fire while I was writing my essay."
+                }
+            },
+            { "work", new List<string>
+                {
+                    "I couldn’t make the meeting because my {0} crashed.",
+                    "I was late because I rescued a {0} on the freeway.",
+                    "My alarm didn’t go off — blame the {0} update."
+                }
+            },
+            { "dating", new List<string>
+                {
+                    "Sorry I ghosted — my ex kidnapped my {0}.",
+                    "I thought today was {0}, not our date day.",
+                    "I tripped on a {0} and blacked out. True story."
+                }
+            }
+        };
 
         string[] wildcards = { "microwave", "printer", "dog", "hamster", "bacon" };
 
         Random random = new Random();
-        int index = random.Next(wildcards.Length);
-        string selectedWord = wildcards[index]; 
+        var categories = new List<string>(excuseTemplates.Keys);
+        string selectedCategory = categories[random.Next(categories.Count)];
 
-        string excuse = string.Format(template, selectedWord);
+        var templates = excuseTemplates[selectedCategory];
+        string selectedTemplate = templates[random.Next(templates.Count)];
 
+        string selectedWord = wildcards[random.Next(wildcards.Length)];
 
-        //Output 
-        Console.WriteLine("Generated Excuse:");
+        string excuse = string.Format(selectedTemplate, selectedWord);
+
+        Console.WriteLine($"Category: {selectedCategory}");
         Console.WriteLine(excuse);
     }
 }
